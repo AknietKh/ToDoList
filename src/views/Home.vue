715cb314ad66adeebb76325743d6d7645e467ced
Akniet.Kh <akniet.khakimov@mail.ru>
2020-04-12 02:36:42 +0700

    Добавил домашнюю страницу

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main-content">
+      <h1 class="header-text">Добро пожаловать</h1>
+      <p>Планируйте свой день вместе с таск-менеджером!</p>
+      <router-link to="/auth">
+        <button class="app-button">Войти</button>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
+
+<style lang="scss" scoped>
+  .home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    background: url("../assets/bg.png") no-repeat center;
+    background-size: cover;
+  }
+
+  h1 {
+    margin-bottom: 5.6rem;
+  }
+
+  p {
+    width: 46rem;
+    margin-bottom: 5.6rem;
+  }
+</style>
