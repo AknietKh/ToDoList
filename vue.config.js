a1c7ccc475d81c161371474446512d7496fc9e74
Akniet.Kh <akniet.khakimov@mail.ru>
2020-04-13 00:28:21 +0700

    отключил eslint в dev режиме

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,10 @@ module.exports = {
   css: {
     loaderOptions: {
       scss: {
-        prependData: `@import "~@/assets/style/variables.scss";`
-      },
+        prependData: '@import "~@/assets/style/variables.scss";'
+      }
     }
-  }
+  },
+  // Отключает eslint в dev режиме
+  lintOnSave: process.env.NODE_ENV !== 'production'
 }
