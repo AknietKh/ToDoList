65d9c04de38555cc54c9ec99fcd15b50b6b681fa
Akniet.Kh <akniet.khakimov@mail.ru>
2020-04-14 02:24:11 +0700

    Сверстал компонент Empty, который показывается когда нет задач

---
+++ src/components/Empty.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="empty-wrapper">
+    <div class="empty-text">Задачи отсутсвуют. Нажмите, чтобы добавить...</div>
+    <div class="empty-plus">+</div>
+  </div>
+</template>
+
+<style lang="scss">
+ .empty {
+    &-wrapper {
+      height: 10rem;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    &-text {
+      font-family: Montserrat;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 18px;
+      line-height: 28px;
+      letter-spacing: 0.09em;
+      color: #3F3D3D;
+    }
+
+    &-plus {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 2.4rem;
+      height: 2.4rem;
+      border: 1px solid black;
+      border-radius: 50%;
+    }
+ }
+</style>
