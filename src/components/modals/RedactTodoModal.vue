583c529be26a36b56e22d949e45b225d9904b845
Akniet.Kh <akniet.khakimov@mail.ru>
2020-04-15 18:45:09 +0700

    Примерный запрос на редкатирование

--- src/components/modals/RedactTodoModal.vue
+++ src/components/modals/RedactTodoModal.vue
@@ -4,7 +4,7 @@
     <div class="modal__main">
         <label class='modal-field'>
           <span>Новое название:</span>
-          <input type="text" class="modal-field__input">
+          <input type="text" class="modal-field__input" v-model='name'>
         </label>
     </div>
     <div class="modal__footer">
@@ -16,9 +16,38 @@
 <script>
 export default {
   name: 'RedactTodoModal',
+  data: function () {
+    return {
+      name: ''
+    }
+  },
   methods: {
     onRedactTodo () {
+      const todoId = this.$store.getters.getTask.todo.id;
+      console.log(todoId);
+
       this.$store.commit('CHANGE_MODAL_TYPE', '')
+
+      this.$store.commit('CHANGE_MODAL_TYPE', '')
+      axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+      axios({
+        method: 'post',
+        url: `http://31.211.50.217/api/update-list/${todoId}`,
+        data: {
+          name: this.name
+        }
+      })
+        .then(resp => {
+          console.log(resp);
+          // const todos = resp.data
+        })
+        .then(() => {
+          this.$store.dispatch('getNotCompletedTodos')
+        })
+        .catch(err => {
+          const errMessage = err.response.data.message
+          console.log(errMessage);
+        })
     }
   }
 }
