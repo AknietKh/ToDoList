71135280e185189c576db7787747f5bec80bb355
Akniet.Kh <akniet.khakimov@mail.ru>
2020-04-15 18:36:13 +0700

    Отображение логина пользователя

--- src/views/TaskManager.vue
+++ src/views/TaskManager.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="page">
     <header class="task-manager-header">
-    <!-- <div class="user-login">{{this.$store.state.user.name}}</div> -->
+    
     <div class="header-wrapper">
-      <div class="user-login">@login_user</div>
+      <!-- <div class="user-login">@login_user</div> -->
+      <div class="user-login">{{getUser.login}}</div>
       <button class="app-button app-button_small _btn-red"
         @click='logout'
       >
@@ -23,7 +24,7 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+// import { mapActions } from 'vuex'
 import Empty from '../components/Empty'
 import TodoList from '../components/TodoList'
 
@@ -38,9 +39,9 @@ export default {
     Empty,
     TodoList
   },
-  // created: function () {
-  //   this.$store.dispatch('getTodos')
-  // },
+  created: function () {
+    this.$store.dispatch('getNotCompletedTodos')
+  },
   methods: {
     logout () {
       this.$store.dispatch('logout')
@@ -52,6 +53,9 @@ export default {
   computed: {
     getTodos () {
       return this.$store.getters.getTodos
+    },
+    getUser () {
+      return this.$store.getters.getUser
     }
   }
 }
