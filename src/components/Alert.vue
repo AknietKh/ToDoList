4e1bf1bce75729106ce6be454af41b6bcd873940
Akniet.Kh <akniet.khakimov@mail.ru>
2020-04-16 16:12:02 +0700

    Добавил компонент alert

---
+++ src/components/Alert.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app-alert">
+    <span>{{alertText}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+  prop: ['alertTitle']
+}
+</script>
+
+<style lang="scss">
+  .app-alert{
+    box-sizing: border-box;
+    width: 42.7rem;
+    height: auto;
+    padding: 1.5rem;
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 1.8rem;
+    line-height: 2.8rem;
+    letter-spacing: 0.09em;
+    color: #000000;
+  }
+</style>
